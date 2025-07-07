5d577ad7d1a5adfac359ab81d9cd83ed99d99fbc
Tanishq Sethi <tanishqsethi05@gmail.com>
2025-07-08 00:29:08 +0530

    fix : landing ui fix

--- src/components/Landing/Hero.tsx
+++ src/components/Landing/Hero.tsx
@@ -11,7 +11,7 @@ const Hero=()=>{
             // backgroundFill="hsl(var(--background))"
             className="min-h-screen flex flex-col items-center justify-center text-center ">
             <AnimatedGradient/>
-            <h1 className="font-mynabali-serif text-balance text-5xl font-bold sm:text-6xl md:text-8xl lg:leading-[1.2] my-2 bg-gradient-to-br from-black via-zinc-900 to-zinc-800 dark:from-white dark:via-zinc-100 dark:to-zinc-300 bg-clip-text text-transparent">
+            <h1 className="font-mynabali-serif text-balance text-4xl font-bold sm:text-6xl md:text-8xl lg:leading-[1.2] my-2 bg-gradient-to-br from-black via-zinc-900 to-zinc-800 dark:from-white dark:via-zinc-100 dark:to-zinc-300 bg-clip-text text-transparent">
                 RuOk - connect to your emotional self!
             </h1>
             <p className=" font-secondary text-balance text-lg leading-[1.2] lg:leading-none hidden md:block md:text-xl px-48 text-zinc-700 dark:text-zinc-400 my-2 font-semibold">
@@ -20,7 +20,7 @@ const Hero=()=>{
             </p>
             <InteractiveHoverButton
                 onClick={()=>navigate('/main')}
-                className={'text-lg md:text-xl my-2 font-secondary font-medium  border-2 border-zinc-600 dark:border-zinc-800'}
+                className={'text-sm sm:text-base md:text-xl my-2 font-secondary font-medium  border-2 border-zinc-600 dark:border-zinc-800'}
             >
                 Get Started
             </InteractiveHoverButton>
