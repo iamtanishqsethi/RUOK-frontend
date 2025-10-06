18303952191281d7908fc0dfc08811db61255d41
Tanishq Sethi <tanishqsethi05@gmail.com>
2025-10-06 22:45:57 +0530

    fix(constants): update BASE_URL to production endpoint

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 // export const BASE_URL: string = 'http://localhost:8000';
 // export const BASE_URL:string='https://ru-ok-backend.vercel.app'
-// export const BASE_URL:string='https://ruok-backend.onrender.com/api'
-export const BASE_URL:string='/api'
+export const BASE_URL:string='https://ruok-backend.onrender.com/api'
+// export const BASE_URL:string='/api'
 // High Energy Unpleasant Colors
 export const highEnergyUnpleasantPrimary = '#ef1a0a';
 export const highEnergyUnpleasantSecondary = '#6c1d00';
